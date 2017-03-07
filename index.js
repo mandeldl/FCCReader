b4e47ebe9f25c16ff6371d3752dd5ccd921a1200
David Mandel <mandeldl@gmail.com>
2017-03-07 13:32:44 -0500

    linting

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-'use strict';
-var inquirer = require('inquirer');
-var opener = require('opener');
-var xray = require('x-ray');
-var x = xray();
+const inquirer = require('inquirer');
+const opener = require('opener');
+const xray = require('x-ray');
+
+const x = xray();
 
 console.log('Fetching articles');
 
-x('http://medium.freecodecamp.com/', 
-		'.postArticle', 
-		[{
-			title: 'h3',
-			url: '.js-trackedPost > a@href'
-		}])
-	(function (err, data) {
-		const siteChoices = data.map(i => i.title);
-		const question = {
-	 		type: 'list',
-	 		name: 'website',
-	 		message: 'Pick a website',
-	 		choices: siteChoices
-	 		// when: (answer) => data.filter( i => answer === i.title)
-	 	}
-
-		inquirer.prompt(question)
-			.then(function (answer) {
-		 		const website = data.filter(i => i.title === answer.website)[0];
-		 		console.log(`Opening "${website.title}"...`);
-		 		opener(website.url);
-		 	});
-	});
+x('http://medium.freecodecamp.com/', '.postArticle', [{
+  title: 'h3',
+  url: '.js-trackedPost > a@href' }])((err, data) => {
+    const siteChoices = data.map(i => i.title);
+    const question = {
+      type: 'list',
+      name: 'website',
+      message: 'Pick a website',
+      choices: siteChoices,
+      pageSize: 10 };
+    inquirer.prompt(question)
+  .then((answer) => {
+    const website = data.filter(i => i.title === answer.website)[0];
+    console.log(`Opening "${website.title}"...`);
+    opener(website.url);
+  });
+  });
